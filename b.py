c7ee8d536549b4a450a99051ecf9ca06e3fb7701
anagarciasaiz <116553250+anagarciasaiz@users.noreply.github.com>
2023-03-26 18:38:19 +0200

    b

---
+++ b.py
@@ -0,0 +1,24 @@
+class palindromo:
+    def __init__ (self,palabra):
+        self.palabra=palabra
+        
+    def test(self):
+        comprobar = True
+        izquierda= 0
+        derecha= len(self.palabra)-1
+        
+        while izquierda != derecha:
+            if self.palabra[izquierda] == self.palabra[derecha]:
+                izquierda = izquierda + 1
+                derecha = derecha - 1
+            else:
+                comprobar = False
+                break
+        
+        if comprobar == True:
+            return True
+        else:
+            return False 
+            
+p=palindromo("radar".lower())
+print(p.test())
