8c058251710647cf7cfaf175fb95de54cd384491
anagarciasaiz <116553250+anagarciasaiz@users.noreply.github.com>
2023-03-26 19:10:21 +0200

    c

---
+++ c.py
@@ -0,0 +1,6 @@
+#El primer print da como resultado "class '__main__.A" debido al self que definimos anteriormente en z
+#El segundo print da como resultado "False", pues la condicion introducida no es verdadera
+#El tercer print da como resultado 0, puesto que no hemos establecido la longuitud correctamente dentro de z
+#El cuarto print da como resultado 1, puesto que dentro de len(a) hay un elemento
+#El quinto print da como resultado 2, puesto que dentro de len(a,z) hay 2 elementos
+#El sexto y ultimo print da como resultado 3, puesto que dentro de len(Z,1,'z') hay 3 elementos
